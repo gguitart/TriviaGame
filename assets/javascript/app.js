1bc73cdc959e76c9d906f8235438c1fbe4e65ed1
GG Guitart <gertaguitart2019@u.northwestern.edu>
2019-09-12 15:07:56 -0500

    Figured out the finishing page, need to be able to update correct answers and wrong answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,15 @@
 
 var number = 100;
 var intervalId;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+$("#hello").hide();
+
 
 function run() {
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
+
 }
 
 function decrement() {
@@ -20,3 +25,25 @@ function stop() {
 }
 
 run();
+
+function end() {
+    //calculate score
+    //build html stuff to show
+    //append to hello
+
+
+    $(".text").hide();
+    $("#hello").append(
+        "<p>hello!</p>",
+        ("Correct Answers: " + correctAnswers),
+        "<br>",
+        "<br>",
+        ("Incorrect Answers: " + incorrectAnswers)
+
+,);
+    console.log(correctAnswers);
+}
+
+
+
+
