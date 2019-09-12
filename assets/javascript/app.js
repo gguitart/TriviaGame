ac817858c455769e76cf705cc88032b76ea3f93b
GG Guitart <gertaguitart2019@u.northwestern.edu>
2019-09-12 13:04:40 -0500

    most of css, need to fix java to show finishing page

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+
+var number = 100;
+var intervalId;
+
+function run() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+}
+
+function decrement() {
+    number--;
+    $("#show-number").html("<h2>" + number + "</h2>");
+    if (number === 0) {
+        stop();
+        alert("Time Up!");
+    }
+}
+function stop() {
+    clearInterval(intervalId);
+}
+
+run();
